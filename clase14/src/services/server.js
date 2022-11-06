e1e4ec9767052aa329d5113f72228e5e02d6005a
Patricio Agustín Kanaszyc <78945504+PatoK93@users.noreply.github.com>
2022-11-06 15:45:32 -0300

    algunos cambios mas

--- clase14/src/services/server.js
+++ clase14/src/services/server.js
@@ -6,8 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
-
 app.use("/api", mainRouter);
 
 app.use((req, res, next) => {
