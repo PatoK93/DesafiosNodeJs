50f0798c8efc24d379d347fa96e9ec5926f03b95
Patricio Agustín Kanaszyc <78945504+PatoK93@users.noreply.github.com>
2022-11-06 15:10:49 -0300

    cambios finales

--- clase14/src/services/server.js
+++ clase14/src/services/server.js
@@ -1,8 +1,5 @@
 import express from "express";
 import mainRouter from "../routes/index.js";
-import path from "path";
-import { fileURLToPath } from "url";
-import Product from "../classes/product.js";
 
 const app = express();
 
@@ -10,20 +7,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const viewsFolderPath = path.resolve(__dirname, "../../views");
-app.set("views", viewsFolderPath);
-app.set("view engine", "pug");
-
-// app.get("/", async (req, res) => {
-//   const fileName = "products.json";
-//   const file = new Product(fileName);
-//   let productArray = file.getAll();
-//   res.render("Products", { productArray });
-// });
 
 app.use("/api", mainRouter);
 
