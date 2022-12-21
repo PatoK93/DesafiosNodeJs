be4a2a15cccaffbfdeccb21f58ab50d2c6c02eaf
Patricio Agustín Kanaszyc <78945504+PatoK93@users.noreply.github.com>
2022-12-20 22:51:36 -0300

    se sube el desafio de la clase 26 con algunas dudas

---
+++ clase26/src/controllers/user.controllers.js
@@ -0,0 +1,34 @@
+import passport from "passport";
+
+const passportOptions = {
+  badRequestMessage: "Problema con username / password!",
+};
+
+export const signUp = (req, res, next) => {
+  passport.authenticate("signup", passportOptions, (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) return res.status(401).json(info);
+    res.json({ msg: "Registrado con  éxito!" });
+  })(req, res, next);
+};
+
+export const login = (req, res, next) => {
+  passport.authenticate("login", passportOptions, (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) return res.status(401).json(info);
+    res.json({ msg: "Bienvenido!", user: req.user });
+  })(req, res, next);
+};
+
+export const getHome = (req, res) => {
+  res.json(req.session);
+};
+
+export const logOut = (req, res) => {
+  req.logOut();
+  res.json({ message: "Hasta Luego!" });
+};
